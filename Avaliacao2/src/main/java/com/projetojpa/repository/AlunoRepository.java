a118a8db221c9fa64a9236a5b196859e1c70b79b
fifizinho <132359775+fifizinho@users.noreply.github.com>
2024-04-04 15:43:14 -0300

    Add files via upload

---
+++ Avaliacao2/src/main/java/com/projetojpa/repository/AlunoRepository.java
@@ -0,0 +1,8 @@
+package com.projetojpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetojpa.entities.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+}
